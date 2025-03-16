def6af403061a37fc0b17f9829616c63922a2f2c
SSaliouDiakhate23 <serignesalioudiakhate97@gmail.com>
2025-03-16 15:09:59 +0100

    Shuflling and Quiz Logic

--- src/Components/Quizz.jsx
+++ src/Components/Quizz.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import { useState } from "react";
 import QUESTIONS from "../assets/Questions";
+import quizzCompletedImg from "../assets/quiz-complete.png";
 
 function Quizz() {
-  console.log(QUESTIONS);
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  // console.log(activeQuestionIndex, userAnswers);
+
+  const quizzIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
   const handleAnswerClick = (answer) => {
     setUserAnswers((prev) => {
       return [...prev, answer];
     });
   };
+
+  if (quizzIsCompleted) {
+    return (
+      <>
+        <div id="summary">
+          <img src={quizzCompletedImg} alt="Quiz completed" />
+          <h2>Quizz Completed</h2>
+        </div>
+      </>
+    );
+  }
+
+  const suffleArray = [...QUESTIONS[activeQuestionIndex].answers];
+  suffleArray.sort(() => Math.random() - 0.5);
+
   return (
     <>
       <div id="quiz">
         <div className="question" style={{ textAlign: "center" }}>
           <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
           <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => {
+            {suffleArray.map((answer, index) => {
               return (
                 <li key={index} className="answer">
                   <button onClick={() => handleAnswerClick(answer)}>
