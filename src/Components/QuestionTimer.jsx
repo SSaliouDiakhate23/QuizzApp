61ba57d3be757e2508457edfd5a74d3158a75749
SSaliouDiakhate23 <serignesalioudiakhate97@gmail.com>
2025-03-27 21:38:27 +0100

    debug

--- src/Components/QuestionTimer.jsx
+++ src/Components/QuestionTimer.jsx
@@ -4,10 +4,12 @@ function QuestionTimer({ time, onTimeOut }) {
   const [remainingTime, setRemainingTime] = useState(time);
 
   useEffect(() => {
+    console.log("SETTING TIME OUT");
     setTimeout(onTimeOut, time);
   }, [time, onTimeOut]);
 
   useEffect(() => {
+    console.log("SETTING INTERVALLE");
     setInterval(() => {
       setRemainingTime((prev) => prev - 100);
     }, 100);
